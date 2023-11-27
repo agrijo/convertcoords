94c41ecd91a8685c668aa7efa6a3da7e792d321e
agrijo <98188675+agrijo@users.noreply.github.com>
2023-11-27 16:06:43 -0300

    update#1
    updating the entire script

--- transcord_final.py
+++ transcord_final.py
@@ -3,6 +3,7 @@ Created on Tue Jul  4 20:03:13 2023
 
 @author: agrijo
 """
+# -*- coding: latin-1 -*-
 
 import tkinter as tk
 from tkinter import filedialog
@@ -10,6 +11,11 @@ import pandas as pd
 from pyproj import Proj, transform
 import utm
 import sys
+from PIL import Image, ImageTk
+import os 
+
+
+basedir = r'C:\Users\agrijo\Desktop\transcord'
 
 epsg_options = {
     "4326": "WGS84",
@@ -17,22 +23,6 @@ epsg_options = {
     "4618":"SAD69",
     "4674":"SIRGAS 2000",
     "4326":"WGS84",
-    "22521":"Corrego Alegre/UTM Zona 21S",
-    "22522":"Corrego Alegre/UTM Zona 22S",
-    "22523":"Corrego Alegre/UTM Zona 23S",
-    "22524":"Corrego Alegre/UTM Zona 24S",
-    "22525":"Corrego Alegre/UTM Zona 25S",
-    "29168":"SAD69/UTM Zona 18N",
-    "29188":"SAD69/UTM Zona 18S",
-    "29169":"SAD69/UTM Zona 19N",
-    "29189":"SAD69/UTM Zona 19S",
-    "29170":"SAD69/UTM Zona 20N",
-    "29190":"SAD69/UTM Zona 20S",
-    "29191":"SAD69/UTM Zona 21S",
-    "29192":"SAD69/UTM Zona 22S",
-    "29193":"SAD69/UTM Zona 23S",
-    "29194":"SAD69/UTM Zona 24S",
-    "29195":"SAD69/UTM Zona 25S",
     "31972":"SIRGAS 2000/UTM Zona 18N",
     "31978":"SIRGAS 2000/UTM Zona 18S",
     "31973":"SIRGAS 2000/UTM Zona 19N",
@@ -58,6 +48,17 @@ epsg_options = {
 
     # Adicione mais opções de EPSG e seus rótulos explicativos aqui
     }
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 def transform_coordinates(lat, lon, from_epsg, to_epsg):
     in_proj = Proj(init=f"epsg:{from_epsg}")
     out_proj = Proj(init=f"epsg:{to_epsg}")
@@ -127,15 +128,15 @@ def transform_file():
         return
 
     try:
-        df = pd.read_csv(file_path, header=0, sep=";")
-        df["Nova Latitude"] = ""
-        df["Nova Longitude"] = ""
-        df["Nova Latitude (GMS)"] = ""
-        df["Nova Longitude (GMS)"] = ""
-        df["Nova Latitude (GMD)"] = ""
-        df["Nova Longitude (GMD)"] = ""
-        df["Zona"] = ""
-        df["Letra"] = ""
+        df = pd.read_excel(file_path, header = 0)
+        df["Latitude UTM"] = ""
+        df["Longitude UTM"] = ""
+        df["Latitude (GMS)"] = ""
+        df["Longitude (GMS)"] = ""
+        df["Latitude (GMD)"] = ""
+        df["Longitude (GMD)"] = ""
+        df["Zona UTM"] = ""
+        df["Letra UTM"] = ""
         
         for index, row in df.iterrows():
             latitude = row[latitude_col]
@@ -145,18 +146,18 @@ def transform_file():
                 
                 lat, lon = utm.to_latlon(longitude, latitude, int(zoneN), str(zoneL))       
                 easting, northing, zone_number, zone_letter = convert_to_utm(lat, lon)
-                df.at[index, 'Nova Latitude'] = lat
-                df.at[index, 'Nova Longitude'] = lon
+                df.at[index, 'Latitude UTM'] = lat
+                df.at[index, 'Longitude UTM'] = lon
                 lat_deg, lat_min, lat_sec, lon_deg, lon_min, lon_sec, lat_final, lon_final = convert_decimal_degrees_to_gms(
                     lat, lon)
-                df.at[index, 'Nova Latitude (GMS)'] = lat_final
-                df.at[index, 'Nova Longitude (GMS)'] = lon_final
+                df.at[index, 'Latitude (GMS)'] = lat_final
+                df.at[index, 'Longitude (GMS)'] = lon_final
                 lat_deg, lat_min_decimal, lon_deg, lon_min_decimal, lat_final_dec, lon_final_dec = convert_decimal_degrees_to_gmd(
                     lat, lon)
-                df.at[index, 'Nova Latitude (GMD)'] = lat_final_dec
-                df.at[index, 'Nova Longitude (GMD)'] = lon_final_dec
-                df.at[index, 'Zona'] = zone_number
-                df.at[index, 'Letra'] = zone_letter
+                df.at[index, 'Latitude (GMD)'] = lat_final_dec
+                df.at[index, 'Longitude (GMD)'] = lon_final_dec
+                df.at[index, 'Zona UTM'] = zone_number
+                df.at[index, 'Letra UTM'] = zone_letter
 
                 if easting is None or northing is None:
                     # Handle latitude out of range
@@ -164,24 +165,26 @@ def transform_file():
             else:
                 
                 easting, northing, zone_number, zone_letter = convert_to_utm(latitude, longitude)
-                df.at[index, 'Nova Latitude'] = northing
-                df.at[index, 'Nova Longitude'] = easting
+                df.at[index, 'Latitude UTM'] = northing
+                df.at[index, 'Longitude UTM'] = easting
                 lat_deg, lat_min, lat_sec, lon_deg, lon_min, lon_sec, lat_final, lon_final = convert_decimal_degrees_to_gms(
                     latitude, longitude)
-                df.at[index, 'Nova Latitude (GMS)'] = lat_final
-                df.at[index, 'Nova Longitude (GMS)'] = lon_final
+                df.at[index, 'Latitude (GMS)'] = lat_final
+                df.at[index, 'Longitude (GMS)'] = lon_final
                 lat_deg, lat_min_decimal, lon_deg, lon_min_decimal, lat_final_dec, lon_final_dec = convert_decimal_degrees_to_gmd(
                     latitude, longitude)
-                df.at[index, 'Nova Latitude (GMD)'] = lat_final_dec
-                df.at[index, 'Nova Longitude (GMD)'] = lon_final_dec
-                df.at[index, 'Zona'] = zone_number
-                df.at[index, 'Letra'] = zone_letter
+                df.at[index, 'Latitude (GMD)'] = lat_final_dec
+                df.at[index, 'Longitude (GMD)'] = lon_final_dec
+                df.at[index, 'Zona UTM'] = zone_number
+                df.at[index, 'Letra UTM'] = zone_letter
             
             # Rest of the code for GMS and GMD conversions
-            
-        save_file_path = filedialog.asksaveasfilename(title="Salvar arquivo CSV")
+        files = {'Arquivo Excel':'*.xlsx'}
+        save_file_path = filedialog.asksaveasfilename(title="Salvar arquivo XLSX", filetypes = files)
         if save_file_path:
-            df.to_csv(save_file_path, index=False, encoding="cp1252")
+            df.to_excel(save_file_path+'.xlsx', index=False)
+            # implementação necessária pro CSV funcionar corretamente  
+            # encoding="cp1252"
             result_label.config(text="Conversão concluída. Arquivo salvo com sucesso.", foreground="blue")
         else:
             result_label.config(text="Conversão concluída.")
@@ -193,33 +196,55 @@ def quit_app():
     
 def preview_file():
     global file_path
-    file_path = filedialog.askopenfilename(title="Selecione o arquivo CSV")
+    file_path = filedialog.askopenfilename(title="Selecione o arquivo XLSX")
     if file_path:
         try:
-            df = pd.read_csv(file_path, nrows=5, sep=';')
+            df = pd.read_excel(file_path, header = 0)
             columns_preview = ", ".join(df.columns)
-            columns_preview_label.config(text=f"Prévia das Colunas: {columns_preview}", foreground="blue")
+            columns_preview_label.config(text=f"Colunas do Arquivo Selecionado: {columns_preview}", foreground="blue")
         except Exception as e:
             columns_preview_label.config(text=f"Erro ao abrir o arquivo: {str(e)}",foreground="red")
     else:
         columns_preview_label.config(text="Nenhum arquivo selecionado.",foreground="red")
 
+def show_image_in_new_window():
+    
+    new_window = tk.Toplevel(root)  # Crie uma nova janela
+    new_window.title("Brasil UTM")  # Defina um título para a nova janela
+
+    # Carregue a imagem usando a biblioteca PIL (Pillow)
+    image_path = r"C:\Users\agrijo\Documents\GitHub\convertcoords\BrasilUTM.png"
+    image = Image.open(image_path)  # Substitua pelo caminho da sua imagem
+    photo = ImageTk.PhotoImage(image)
+
+    # Crie um rótulo para exibir a imagem na nova janela
+    image_label = tk.Label(new_window, image=photo)
+    image_label.image = photo  # Salve uma referência para evitar que a imagem seja coletada pelo garbage collector
+    image_label.pack()
+
+    close_button = tk.Button(new_window, text="Fechar Figura", command=new_window.destroy)
+    close_button.pack()
+    
 root = tk.Tk()
 root.title("Calculadora de Coordenadas")
+
+
+
 # root.geometry("400x400")
 
 # Labels
 tk.Label(root, text="Coluna Latitude:").grid(row=0, column=0, sticky="e")
 tk.Label(root, text="Coluna Longitude:").grid(row=1, column=0, sticky="e")
-tk.Label(root, text="EPSG de Origem:").grid(row=2, column=0, sticky="e")
-tk.Label(root, text="EPSG de Destino:").grid(row=3, column=0, sticky="e")
+tk.Label(root, text="EPSG de Entrada:").grid(row=2, column=0, sticky="e")
+tk.Label(root, text="EPSG de Saída:").grid(row=3, column=0, sticky="e")
 tk.Label(root, text="Tipo de Coordenada:").grid(row=4, column=0, sticky="e")
-tk.Label(root, text="Número da Zona:").grid(row=5, column=0, sticky="e")
-tk.Label(root, text="Letra da Zona:").grid(row=6, column=0, sticky="e")
+tk.Label(root, text="Número da Zona UTM:").grid(row=5, column=0, sticky="e")
+tk.Label(root, text="Letra da Zona UTM:").grid(row=6, column=0, sticky="e")
+
 result_label = tk.Label(root, text="")
 result_label.grid(row=11, column=0, columnspan=2, pady=10)
 
-columns_preview_label = tk.Label(root, text="Prévia das Colunas: ")
+columns_preview_label = tk.Label(root, text="Colunas do Arquivo: ")
 columns_preview_label.grid(row=10, column=0, columnspan=2, pady=10)
 
 # Entry Fields
@@ -248,10 +273,13 @@ input_type_dropdown = tk.OptionMenu(root, input_type_var, "UTM", "Graus Decimais
 input_type_dropdown.grid(row=4, column=1)
 
 # Buttons
-preview_button = tk.Button(root, text="Pré-visualizar Arquivo", command=preview_file)
+preview_button = tk.Button(root, text="Abrir Arquivo", command=preview_file)
 preview_button.grid(row=8, column=0, pady=10)
 
-transform_button = tk.Button(root, text="Transformar", command=transform_file)
+show_image_button = tk.Button(root, text="Brasil UTM", command=show_image_in_new_window)
+show_image_button.grid(row=4, column=2, pady=10)
+
+transform_button = tk.Button(root, text="Transformar Coordenadas", command=transform_file)
 transform_button.grid(row=8, column=1, pady=10)
 
 quit_button = tk.Button(root, text="Sair", command=quit_app)
